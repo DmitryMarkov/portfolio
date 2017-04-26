b3367ce5c89392da73eb3532be310f02fc13cffe
DmitryMarkov <forall@stylive.com>
2017-04-26 19:27:55 +0300

    Update 2017-04-26T16:27:48.416Z

--- app.js
+++ app.js
@@ -1,9 +1,7 @@
 ;(function () {
   'use strict'
 
-  setTimeout(() => {
-    document.querySelector('.greating_picture').classList.add('m--show')
-  }, 1000)
+  console.log('script linked')
 })()
 
-//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImFwcC5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0E7QUFDQTtBQUNBIiwiZmlsZSI6ImFwcC5qcyIsInNvdXJjZXNDb250ZW50IjpbIjsoZnVuY3Rpb24gKCkge1xuICAndXNlIHN0cmljdCdcblxuICBzZXRUaW1lb3V0KCgpID0+IHtcbiAgICBkb2N1bWVudC5xdWVyeVNlbGVjdG9yKCcuZ3JlYXRpbmdfcGljdHVyZScpLmNsYXNzTGlzdC5hZGQoJ20tLXNob3cnKVxuICB9LCAxMDAwKVxufSkoKVxuIl19
+//# sourceMappingURL=data:application/json;charset=utf8;base64,eyJ2ZXJzaW9uIjozLCJzb3VyY2VzIjpbImFwcC5qcyJdLCJuYW1lcyI6W10sIm1hcHBpbmdzIjoiQUFBQTtBQUNBO0FBQ0E7QUFDQTtBQUNBO0FBQ0EiLCJmaWxlIjoiYXBwLmpzIiwic291cmNlc0NvbnRlbnQiOlsiOyhmdW5jdGlvbiAoKSB7XG4gICd1c2Ugc3RyaWN0J1xuXG4gIGNvbnNvbGUubG9nKCdzY3JpcHQgbGlua2VkJylcbn0pKClcbiJdfQ==
